c8424fbf477a042f410e6bd667bdb117c772a463
Pushkar Patel <pushkar.umrao@gmail.com>
2024-05-31 19:38:05 +0530

    Design changes in circuit definition

--- src/gates/circuit.rs
+++ src/gates/circuit.rs
@@ -1,53 +1,64 @@
 
 use crate::gates::logic_gates::LogicGate;
 
+pub struct Component{
+    gate: LogicGate,
+    input_indices: Vec<usize>,
+    output_index: usize,
+}
+
+impl Component{
+    fn fire(&self, outputs: &Vec<bool>)->bool{
+        let pins: Vec<bool> = self.input_indices.iter().map(|&i| outputs[i]).collect();
+        let result = self.gate.evaluate(&pins);
+        println!("Inputs = {:?}, op_index = {} | output = {}",  pins, self.output_index,result);
+        println!("____________________________________________________\n");
+        result
+    }
+}
+
 pub struct Circuit {
-    gates: Vec<(LogicGate, Vec<usize>)>, // (gate, input indices)
-    inputs: Vec<bool>
+    components: Vec<Component>, // (gate, input indices)
+    pins: Vec<bool>
 }
 
 impl Circuit {
     pub fn new() -> Self {
         Circuit {
-            gates: Vec::new(),
-            inputs: Vec::new()
+            components: Vec::new(),
+            pins: Vec::new()
         }
     }
 
-    // TODO: implement a join function
-    //		- create branch
-	//  		- function add input and add gate
-	// 		- join branch
-	//  		- add the outputs of two branch via a gate
-
     pub fn add_input(&mut self, input: bool) {
-        self.inputs.push(input);
+        self.pins.push(input);
     }
 
     pub fn add_gate(&mut self, gate: LogicGate, input_indices: Vec<usize>) {
-        self.gates.push((gate, input_indices));
+        let component = Component{
+            gate,
+            input_indices,
+            output_index: (self.components.len()+self.pins.len())
+        };
+        self.components.push(component);
     }
 
-
     pub fn evaluate(&self, cycles: usize) -> Vec<bool> {
-        let mut outputs = vec![false; self.inputs.len() + self.gates.len()];
+        let mut outputs = vec![false; self.pins.len() + self.components.len()];
 
-        // Initialize the outputs with initial inputs
-        outputs[..self.inputs.len()].copy_from_slice(&self.inputs);
+        // Initialize the outputs with initial pins
+        outputs[..self.pins.len()].copy_from_slice(&self.pins);
+        
         for clk in 0..cycles-1 {
             let mut new_outputs = outputs.clone(); // start a new transaction at each cycle
 
-            for (index, (gate, input_indices)) in self.gates.iter().enumerate() {
-                let inputs: Vec<bool> = input_indices.iter().map(|&i| outputs[i]).collect();
-                let result = gate.evaluate(&inputs);
-                new_outputs[self.inputs.len() + index] = result;
+            // TODO: parallelize this for loop
+            for component in &self.components{
                 
-                println!("\nPropogating gate {} at index {}", gate.info(), index);
-                println!("Inputs = {:?} | output = {}",  inputs, result);
-                println!("change at index {}, new val {:?}", self.inputs.len() + index-1, new_outputs);
-                println!("____________________________________________________\n")
-                // result for ith gate is stored at [inputs.len()+index_of_gate]
-            }
+                println!("\nPropogating gate {}", component.gate.info());
+                new_outputs[component.output_index] = component.fire(&outputs);
+                
+            };
 
             outputs = new_outputs; // only commit after one cycle
             println!("---------- {} cycles complete ------------ : evaluated to {:?}", clk+1, outputs);
